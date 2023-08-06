9d4027db2f20bd6d913184dd280542c42921ed26
srinivassura18 <141510214+srinivassura18@users.noreply.github.com>
2023-08-06 13:59:53 +0530

    Update demo.java
    missed colon.

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 class demo{
 	public static void main(String[] args){
-	System.out.println("This is a demo program."):
+	System.out.println("This is a demo program.");
 	System.out.println("Git is very is to operate");
 	}
 }
